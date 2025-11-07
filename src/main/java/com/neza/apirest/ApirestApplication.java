62798aff028d88ee1fea5ac40ee149190b01aba6
Nezareth <nezavirg159@gmail.com>
2025-11-06 23:20:01 -0500

    se agrega un mensaje por consola

--- src/main/java/com/neza/apirest/ApirestApplication.java
+++ src/main/java/com/neza/apirest/ApirestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApirestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(ApirestApplication.class, args);
 	}
 
